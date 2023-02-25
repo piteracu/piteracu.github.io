c2d85d71560e0fdaf3a28e5e16199b00946f2849
piteracu <acufernandoesteban@gmail.com>
2023-02-25 01:19:00 -0300

    Esta es la versión actualizada hasta el momento

--- js/script.js
+++ js/script.js
@@ -17,3 +17,9 @@ targets.forEach(target => {
 
     })
 })
+
+
+
+document.getElementById('btn_magico').addEventListener('click', function(){
+    document.getElementById('magico').innerHTML = "Mas info de mi.....Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse luctus, turpis a elementum consequat, diam mi laoreet lorem, nec mattis leo purus id erat. Proin erat ligula, semper id iaculis eget, tincidunt id neque. Maecenas at libero at odio convallis ullamcorper ac luctus justo. Pellentesque ac ligula justo. Ut ullamcorper tellus id aliquet porttitor. Aliquam nisi leo, bibendum sed aliquam at, lacinia eget erat. Etiam a mollis diam, et hendrerit leo. Pellentesque iaculis ligula eu pharetra dignissim. Sed scelerisque erat sit amet pretium scelerisque. Nunc id sapien est. Praesent sed ultrices orci, et pretium purus. Suspendisse ornare dictum turpis a pulvinar. Maecenas orci nunc, suscipit vel dignissim malesuada, tempus sit amet ante. Nam facilisis est augue, eget fringilla nibh fringilla in. In vel diam at arcu suscipit dictum. Mauris dapibus ligula nec massa pretium interdum"
+})
